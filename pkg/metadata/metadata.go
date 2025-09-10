7d0bd9aea919c8b85b0f5c18a2a1b864976c7323
Brett Whitty <brettwhitty@gmail.com>
2025-09-10 02:48:20 -0400

    feat: Add composite ID generation to pkg/utils and update FileMetadata

--- pkg/metadata/metadata.go
+++ pkg/metadata/metadata.go
@@ -6,49 +6,72 @@ import (
 )
 
 type FileMetadata struct {
-        ID       string                 `json:"_id"`
-        FilePath string                 `json:"filePath"`
-        Size     int64                  `json:"size"`
-        ModTime  string                 `json:"modTime"`
-        Extra    map[string]interface{} `json:"-"`
+	ID       string                 `json:"_id"`    // Unique document ID (the fingerprint)
+	IDString string                 `json:"idString"` // Composite string used to generate ID
+	HostID   string                 `json:"hostID"`   // ID of the host where the file was indexed
+	FilePath string                 `json:"filePath"`
+	Size     int64                  `json:"size"`
+	ModTime  string                 `json:"modTime"`
+	BLAKE3   string                 `json:"blake3"`   // BLAKE3 hash of the file content
+	Extra    map[string]interface{} `json:"-"`
 }
 
 func (fm *FileMetadata) UnmarshalJSON(data []byte) error {
-        var tmp map[string]interface{}
-        if err := json.Unmarshal(data, &tmp); err != nil {
-                return err
-        }
-        if id, ok := tmp["_id"].(string); ok {
-                fm.ID = id
-        }
-        if fp, ok := tmp["filePath"].(string); ok {
-                fm.FilePath = fp
-        }
-        if size, ok := tmp["size"].(float64); ok {
-                fm.Size = int64(size)
-        }
-        if mt, ok := tmp["modTime"].(string); ok {
-                fm.ModTime = mt
-        }
-        delete(tmp, "_id")
-        delete(tmp, "filePath")
-        delete(tmp, "size")
-        delete(tmp, "modTime")
-        fm.Extra = tmp
-        return nil
+	var tmp map[string]interface{}
+	if err := json.Unmarshal(data, &tmp); err != nil {
+		return err
+	}
+
+	// Unmarshal known fields
+	if id, ok := tmp["_id"].(string); ok {
+		fm.ID = id
+	}
+	if idString, ok := tmp["idString"].(string); ok {
+		fm.IDString = idString
+	}
+	if hostID, ok := tmp["hostID"].(string); ok {
+		fm.HostID = hostID
+	}
+	if fp, ok := tmp["filePath"].(string); ok {
+		fm.FilePath = fp
+	}
+	if size, ok := tmp["size"].(float64); ok {
+		fm.Size = int64(size)
+	}
+	if mt, ok := tmp["modTime"].(string); ok {
+		fm.ModTime = mt
+	}
+	if blake3, ok := tmp["blake3"].(string); ok {
+		fm.BLAKE3 = blake3
+	}
+
+	// Populate Extra map with unknown fields
+	fm.Extra = make(map[string]interface{})
+	for k, v := range tmp {
+		switch k {
+		case "_id", "idString", "hostID", "filePath", "size", "modTime", "blake3":
+			// Skip known fields
+		default:
+			fm.Extra[k] = v
+		}
+	}
+	return nil
 }
 
 func (fm *FileMetadata) MarshalJSON() ([]byte, error) {
-        m := map[string]interface{}{
-                "_id":      fm.ID,
-                "filePath": fm.FilePath,
-                "size":     fm.Size,
-                "modTime":  fm.ModTime,
-        }
-        for k, v := range fm.Extra {
-                if _, exists := m[k]; !exists {
-                        m[k] = v
-                }
-        }
-        return json.Marshal(m)
+	m := map[string]interface{}{
+		"_id":      fm.ID,
+		"idString": fm.IDString,
+		"hostID":   fm.HostID,
+		"filePath": fm.FilePath,
+		"size":     fm.Size,
+		"modTime":  fm.ModTime,
+		"blake3":   fm.BLAKE3,
+	}
+	for k, v := range fm.Extra {
+		if _, exists := m[k]; !exists { // Only add if not a known field
+			m[k] = v
+		}
+	}
+	return json.Marshal(m)
 }
--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -1,2 +1,54 @@
-package main
+package utils
 
+import (
+	"path/filepath"
+
+	"github.com/adrg/xdg"
+	"github.com/denisbrodbeck/machineid"
+	"github.com/google/uuid"
+	"encoding/base64"
+	"log"
+	"fmt"
+)
+
+// DefaultBoltDBPath returns the system-appropriate default DB path.
+func DefaultBoltDBPath() string {
+	// Use XDG data home; for Windows or macOS this resolves appropriately.
+	dataHome := xdg.DataHome
+	return filepath.Join(dataHome, "indexer", "indexer.db")
+}
+
+// XDGDataHome returns the XDG data home directory.
+func XDGDataHome() string {
+	return xdg.DataHome
+}
+
+var HostID string
+
+// SetHostID allows the value to be overridden by config value
+func SetHostID(cfgHost ...string) {
+	// if a string was provided, use that
+	if (len(cfgHost) == 1) {
+		HostID = cfgHost[0]
+	} else {
+		// otherwise we'll use the machineid library
+		id, err := machineid.ProtectedID("DreamFS")
+		if err != nil {
+			log.Fatal(err)
+		}
+		HostID = id
+	}
+}
+
+// GenerateUUID generates a 'v5 UUID' for a string value
+func GenerateUUID(data string) string {
+	// instantiate the UUID object and return as a string
+	uuid:= uuid.NewSHA1(uuid.NameSpaceURL, []byte(data))
+	return uuid.String()
+}
+
+// ShortenString uses base64 encoding to shorten a string
+func ShortenString(data string) string {
+	// URL safe encoding; should be 22 chars in length
+	return base64.RawURLEncoding.EncodeToString([]byte(data))
+}
