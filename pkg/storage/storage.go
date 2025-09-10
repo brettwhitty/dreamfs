c8ad936f36c968913d306a40aa43ce6ab59933cd
Brett Whitty <brettwhitty@gmail.com>
2025-09-10 02:45:54 -0400

    feat: Add CacheWriter to pkg/storage

--- pkg/storage/storage.go
+++ pkg/storage/storage.go
@@ -3,8 +3,10 @@ package storage
 import (
 	"encoding/json"
 	"fmt"
+	"log"
 	"os"
 	"path/filepath"
+	"sync"
 	"time"
 
 	bolt "go.etcd.io/bbolt"
@@ -70,4 +72,98 @@ func (ps *PersistentStore) GetAll() ([]metadata.FileMetadata, error) {
                 })
         })
         return results, err
+}
+
+//
+// CACHE WRITER (In-Memory Caching to Batch Writes)
+//
+type CacheWriter struct {
+	ps            *PersistentStore // Reference to PersistentStore in this package
+	ch            chan metadata.FileMetadata // Reference to FileMetadata from metadata package
+	batchSize     int
+	flushInterval time.Duration
+	flushNowCh    chan struct{}
+	quit          chan struct{}
+	wg            sync.WaitGroup
+}
+
+func NewCacheWriter(ps *PersistentStore, batchSize int, flushInterval time.Duration) *CacheWriter {
+	cw := &CacheWriter{
+		ps:            ps,
+		ch:            make(chan metadata.FileMetadata, batchSize*2),
+		batchSize:     batchSize,
+		flushInterval: flushInterval,
+		flushNowCh:    make(chan struct{}),
+		quit:          make(chan struct{}),
+	}
+	cw.wg.Add(1)
+	go cw.run()
+	return cw
+}
+
+func (cw *CacheWriter) run() {
+	var batch []metadata.FileMetadata
+	timer := time.NewTimer(cw.flushInterval)
+	for {
+		select {
+		case meta := <-cw.ch:
+			batch = append(batch, meta)
+			if len(batch) >= cw.batchSize {
+				cw.flush(batch)
+				batch = nil
+				if !timer.Stop() {
+					<-timer.C
+				}
+				timer.Reset(cw.flushInterval)
+			}
+		case <-timer.C:
+			if len(batch) > 0 {
+				cw.flush(batch)
+				batch = nil
+			}
+			timer.Reset(cw.flushInterval)
+		case <-cw.flushNowCh:
+			if len(batch) > 0 {
+				cw.flush(batch)
+				batch = nil
+			}
+		case <-cw.quit:
+			if len(batch) > 0 {
+				cw.flush(batch)
+			}
+			return
+		}
+	}
+}
+
+func (cw *CacheWriter) flush(batch []metadata.FileMetadata) {
+	err := cw.ps.db.Update(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte("metadata"))
+		for _, meta := range batch {
+			data, err := json.Marshal(meta)
+			if err != nil {
+				return err
+			}
+			if err := b.Put([]byte(meta.ID), data); err != nil {
+				return err
+			}
+		}
+		return nil
+	})
+	if err != nil {
+		log.Printf("CacheWriter flush error: %v", err)
+	}
+}
+
+func (cw *CacheWriter) Write(meta metadata.FileMetadata) {
+	cw.ch <- meta
+}
+
+func (cw *CacheWriter) FlushNow() {
+	cw.flushNowCh <- struct{}{}
+}
+
+func (cw *CacheWriter) Close() {
+	close(cw.quit)
+	cw.wg.Wait()
 }
