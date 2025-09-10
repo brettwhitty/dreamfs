e891e611ab82afcbb36e119b469335806400a27b
Brett Whitty <brettwhitty@gmail.com>
2025-09-10 02:43:44 -0400

    feat: Add InitConfig to pkg/config

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1,2 +1,31 @@
-package main
+package config
 
+import (
+	"path/filepath"
+
+	"github.com/fatih/color"
+	"github.com/spf13/viper"
+
+	"gnomatix/dreamfs/v2/pkg/utils"
+)
+
+// ------------------------
+// Configuration and CLI Setup
+// ------------------------
+
+func InitConfig(cfgFile string) {
+	if cfgFile != "" {
+		viper.SetConfigFile(cfgFile)
+	} else {
+		xdgConfigDir := utils.XDGDataHome()
+		viper.AddConfigPath(xdgConfigDir)
+		viper.SetConfigName("indexer")
+		viper.SetConfigType("json")
+	}
+	viper.AutomaticEnv()
+	if err := viper.ReadInConfig(); err == nil {
+		color.Magenta("Using config file: %s", viper.ConfigFileUsed())
+	} else {
+		color.Yellow("No config file found; using defaults and flags")
+	}
+}
