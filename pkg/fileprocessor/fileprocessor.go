c7ea6c60d1d7945189fe7769264637eef892871d
Brett Whitty <brettwhitty@gmail.com>
2025-09-10 02:34:01 -0400

    feat: Add getPartitions to pkg/fileprocessor

--- pkg/fileprocessor/fileprocessor.go
+++ pkg/fileprocessor/fileprocessor.go
@@ -1,2 +1,34 @@
-package main
+package fileprocessor
 
+import (
+	"sync"
+	"time"
+
+	"github.com/shirou/gopsutil/disk"
+)
+
+// ------------------------
+// Filesystem Partition Caching for Canonicalization
+// ------------------------
+
+var (
+	partitionsCache     []disk.PartitionStat
+	partitionsCacheTime time.Time
+	cacheMutex          sync.Mutex
+	cacheDuration       = 5 * time.Minute
+)
+
+func GetPartitions() ([]disk.PartitionStat, error) {
+	cacheMutex.Lock()
+	defer cacheMutex.Unlock()
+	if time.Since(partitionsCacheTime) < cacheDuration && partitionsCache != nil {
+		return partitionsCache, nil
+	}
+	parts, err := disk.Partitions(true)
+	if err != nil {
+		return nil, err
+	}
+	partitionsCache = parts
+	partitionsCacheTime = time.Now()
+	return parts, nil
+}
